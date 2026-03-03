41574f64c5e422a9b5d7c07101415c0ef28d551f
Anand Kulkarni <anankulk@qti.qualcomm.com>
2026-03-03 11:59:21 +0530

    Test_CI : Test change for validating ABI_checker
    Signed-off-by: Anand Kulkarni <anankulk@qti.qualcomm.com>

---
+++ src/test_abi_violation.c
@@ -0,0 +1,12 @@
+// Copyright (c) 2024, Qualcomm Innovation Center, Inc. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+
+#include <stdio.h>
+
+/* 
+ * Dummy function for testing CI workflow.
+ * This file is intended to test if the ABI checker runs.
+ */
+void test_abi_logic_execution(void) {
+    printf("This is a test artifact for ABI validation.\n");
+}
